39da40a0004f73b6d39c75de4b576386946bec02
ArtT14 <lamferme@gmail.com>
2022-06-29 21:46:45 -0700

    Finished Hierarchical clustering, starting logistic regression

--- w3-schools/6-ml-hiearchical-clustering.py
+++ w3-schools/6-ml-hiearchical-clustering.py
@@ -1,3 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering
+
+
 """
 HIERCHICAL CLUSTERING:
 Hierarchical clustering is an unsupervised learning method for clustering data points.
@@ -7,3 +13,25 @@ This method can be used on any data to visualize and interpret the relationship 
 
 Here we will use hierarchical clustering to group data points and visualize the clusters using both a dendrogram and scatter plot.
 """
+
+# DATA
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+
+# COMPUTE ward linkage using euclidean distance & visualize the dendogram
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+
+# Clustering like above but using scatter plot
+hierarchical_cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
+labels = hierarchical_cluster.fit_predict(data)
+
+plt.scatter(x, y, c=labels)
+plt.show()
