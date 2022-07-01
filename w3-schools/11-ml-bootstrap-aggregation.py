e6b5e441fb84f2c2b32a024fc75747417b8ffd1c
ArtT14 <lamferme@gmail.com>
2022-06-30 21:19:59 -0700

    Added files for the last 4 concepts

---
+++ w3-schools/11-ml-bootstrap-aggregation.py
@@ -0,0 +1,5 @@
+"""
+BOOTSTRAP AGGREGATION (BAGGING):
+an ensembling method that attempts to resolve overfitting for classification or regression problems.
+Bagging aims to improve the accuracy and performance of machine learning algorithms.
+"""
