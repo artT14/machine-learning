8c55df9b262bca1561f367198ecf4502b5dd1eee
artT14 <lamferme@gmail.com>
2022-08-21 00:14:06 -0700

    Done w/ optimization

--- pytorch tut/scr2 - official/basics/06_optimization.py
+++ pytorch tut/scr2 - official/basics/06_optimization.py
@@ -50,7 +50,56 @@ class NeuralNetwork(nn.Module):
         return logits
 
 model = NeuralNetwork()
+
 # HYPER PARAMETERS - control the optimization process
 learning_rate = 1e-3
 batch_size = 64 # num of data samples propogated thru NN before parameters are updated
-epochs = 5
+epochs = 10
+
+# OPTIMIZATION LOOP
+#   - Train Loop: iterate over training dataset & try to converge to optimal parameters
+#   - Test Loop: iterate over test dataset to check if model performance is improving
+
+# LOSS FUNCTION: goal is to minimize error in this function
+loss_fn = nn.CrossEntropyLoss()
+
+# OPTIMIZER: process of adjusting model parameters to reduce model error in each training step
+optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)
+
+# FULL IMPLEMENTATION
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X,y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred,y)
+        # Backpropogation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
